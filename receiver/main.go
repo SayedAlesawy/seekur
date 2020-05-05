1be3623f809bec9816f9d12f48b84f429f92b89d
SayedAlesawy <sayedalesawy14@gmail.com>
2020-05-05 23:11:31 +0200

    implement receiver

--- receiver/main.go
+++ receiver/main.go
@@ -1,20 +1,23 @@
 package main
 
 import(
-	"github.com/SayedAlesawy/seekur/drivers/tcp"
-	"github.com/pebbe/zmq4"
 	"log"
+	"bytes"
+
+	"github.com/pebbe/zmq4"
+	"github.com/SayedAlesawy/seekur/drivers/tcp"
+	"github.com/SayedAlesawy/seekur/rsa"
 )
 
-func main() {
-	ip := "127.0.0.1"
-	port := "9902"
-	logSign := "[Receiver]"
+var logSign = "[Receiver]"
+var ip = "127.0.0.1"
+var port = "9902"
 
+func main() {
 	// Construct the communicaiton endpoint with receiver
 	endpoint := tcp.BuildConnectionString(ip, port)
 
-	// Acquire a request connection 
+	// Acquire a request connection
 	connection, err := tcp.NewConnection(zmq4.SUB, "")
 	if err {
 		log.Fatal(logSign, "Failed to init a reply socket")
@@ -22,15 +25,32 @@ func main() {
 
 	// Connect to the receiver endpoint
 	connection.Connect(endpoint)
+	log.Println(logSign, "Listening...")
 
-	for {
-		msg, err := connection.RecvString(0)
-
-		if err{
-			log.Println(logSign, "Failed to receive message")
-			continue
-		}
-
-		log.Println(logSign, "Received: ", msg)
+	// Receive private key
+	priv, err := connection.RecvBytes(0)
+	if err{
+		log.Println(logSign, "Failed to receive message")
 	}
+
+	log.Println(logSign, "Received Private key")
+
+	// Decode private key
+	privKey, err := rsa.DecodePrivKey(priv)
+	if err{
+		log.Fatal(logSign, "Error while decoding private key")
+	}
+
+	// Receive the encrypted message
+	recvMsg, err := connection.RecvBytes(0)
+	if err{
+		log.Println(logSign, "Failed to receive message")
+	}
+
+	// Decrypt the received message
+	decyrptedMsg := rsa.Decrypt(&privKey, recvMsg)
+
+	// Print plain text after decryption
+	plainTextMsg := bytes.NewBuffer(decyrptedMsg.Bytes()).String()
+	log.Println(logSign, "Received message: ", plainTextMsg)
 }
